8a325fb06556b0c5b9f4c2158b58a732f341639c
firebfm <firebfm@gmail.com>
2022-01-26 15:47:23 -0500

    creating tag entity

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -7,6 +7,8 @@ const config: ConnectionOptions = {
   username: 'postgres',
   password: 'test',
   database: 'mediumclone',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
 };
 
 export default config;
